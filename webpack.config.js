5f2be1f304beee4d681dcfb7a8de206df5b916cb
Austin Ezell <austin.r.ezell@gmail.com>
2017-05-22 19:17:23 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: __dirname + '/dist/',
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules\/|bower_components\/)/,
+      loader: 'babel?presets[]=es2015'
+    }]
+  }
+};
